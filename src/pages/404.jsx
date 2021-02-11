8b8f1ee3d28d7413383d121bf8c5d9771bf60096
EduRibeiro00 <eribeiro306@gmail.com>
2021-02-11 11:48:05 +0000

    Added 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,54 +1,42 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import React from 'react'
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
+import Footer from '../components/Footer'
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
-const NotFoundPage = () => {
+const NotFoundPage = ({ data }) => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
+    <main className="bg-gradient-to-b from-gray-900 to-blue-600 flex flex-col items-center justify-center h-screen">
+		<div className="flex flex-col items-center max-w-md bg-white px-10 py-3 shadow-2xl rounded-xl">
+			<h1 className="text-bold text-7xl text-center mb-3">
+				404
+			</h1>
+			<p className="font-bold text-center">
+				Hmm...this page doesn't seem to exist 🧐
+			</p>
+			<a className="mt-10 mb-3 btn-blue" href="/">
+				Back to Main Page
+			</a>
+		</div>
+		<div className="absolute bottom-0">
+		<Footer links={data.dataJson}/>
+		</div>
     </main>
   )
 }
 
 export default NotFoundPage
+
+export const query = graphql`
+	query {
+		dataJson {
+			email {
+				link
+			}
+			github {
+				link
+			}
+			linkedin {
+				link
+			}
+		}
+	}          
+`
