e38485f9398ea3a6453a04049241c4ecf46ea706
INESC <eduardo.ribeiro@inesctec.pt>
2021-03-28 15:42:44 +0100

    Added RSS Feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,12 +64,91 @@ module.exports = {
           }
         ]
       }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Eduardo Ribeiro`,
+        short_name: `Eduardo Ribeiro`,
+        start_url: `/`,
+        background_color: `#e5e7eb`,
+        theme_color: `#1E40B8`,
+        display: `standalone`,
+        icon: `static/favicon.ico`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                  defaultTitle: title
+                  titleTemplate
+                  defaultDescription: description
+                  siteUrl
+                  defaultImage: image
+                  author
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.post_date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                })
+              })
+            },
+            query: `
+            {
+              allMarkdownRemark(
+                  sort: { fields: [frontmatter___post_date], order: DESC },
+                  filter: {
+                      frontmatter: {
+                          blogpost: {eq: true}
+                      }
+                  }
+              ) {
+                  totalCount
+                  edges {
+                      node {
+                          id
+                          frontmatter {
+                              title
+                              post_date(formatString: "DD MMMM, YYYY")
+                          }
+                          fields {
+                              slug
+                          }
+                          excerpt(format: PLAIN, pruneLength: 100)
+                          html
+                          timeToRead
+                      }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "Eduardo Ribeiro's Blog",
+          },
+        ],
+      },
+    },
   ],
   siteMetadata: {
     title: "Eduardo Ribeiro",
     titleTemplate: "%s · Eduardo Ribeiro",
     description: "Personal Website and Blog of Eduardo Ribeiro",
+    author: "Eduardo Ribeiro",
     siteUrl: "https://eduardocribeiro.com",
     image: "/images/profile-photo.jpg"
   },
--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -12,7 +12,8 @@ const Blog = ({ data }) => {
             <SEO title="Blog" />
             <h1 className="text-5xl">Blog</h1>
             <p className="my-4">Hello! In this page you can see my blog, with every blog post I have made so far.<br/> I will mainly talk about my software engineering journey and tech related stuff, but occasionally you might find something related to other hobbies of mine, or just rambling nonsense. <br/>Hope you enjoy. 👌</p>
-            <p className="my-4">I also post on other platforms, so make sure you follow me on <a href={data.dataJson.dev.link}>DEV</a> and <a href={data.dataJson.medium.link}>Medium</a> 😃</p>
+            <p>If you want to stay tuned, this blog is also available as an <a href={data.dataJson.rss_feed}>RSS Feed</a>!</p>
+            <p>I also post on other platforms, so make sure you follow me on <a href={data.dataJson.dev.link}>DEV</a> and <a href={data.dataJson.medium.link}>Medium</a> 😃</p>
             <h2 className="flex items-center mt-8 mb-4 justify-center md:justify-start"><div className="mr-1">All my Blog Posts</div><FaArrowDown /></h2>
             <p className="mb-7 text-center md:text-left">{data.allMarkdownRemark.totalCount} {data.allMarkdownRemark.totalCount === 1 ? 'post' : 'posts'}</p>
             <div className="flex flex-wrap justify-center md:justify-evenly">
@@ -41,6 +42,7 @@ export const query = graphql`
             medium {
                 link
             }
+            rss_feed
         }
         allMarkdownRemark(
             sort: { fields: [frontmatter___post_date], order: DESC },
